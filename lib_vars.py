9e6e29fc32f4cf5ce0fbcfd077f462fc6941ff90
Sergi Maspons <smaspons@bgeo.es>
2023-02-16 15:09:38 +0100

    Add files

---
+++ lib_vars.py
@@ -0,0 +1,12 @@
+schema_name = None
+session_vars = None
+qgis_db_credentials = None
+plugin_name = None
+plugin_dir = None
+user_folder_dir = None
+logger = None
+project_vars = None
+data_epsg = None
+project_epsg = None
+date_format = None
+user_level = None
