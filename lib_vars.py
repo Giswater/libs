a90cd3b0fe6a4f1d35688142237c2ad06a840a62
Sergi Maspons <smaspons@bgeo.es>
2023-06-16 12:42:57 +0200

    Add comments & improve lib_vars

--- lib_vars.py
+++ lib_vars.py
@@ -1,12 +1,38 @@
-schema_name = None
-session_vars = None
-qgis_db_credentials = None
-plugin_name = None
-plugin_dir = None
-user_folder_dir = None
-logger = None
-project_vars = None
-data_epsg = None
-project_epsg = None
-date_format = None
-user_level = None
+schema_name = None                      # Schema name retrieved from QGIS project connection with PostgreSql
+qgis_db_credentials = None              # Instance of class QSqlDatabase (QPSQL) used to manage QTableView widgets
+plugin_name = None                      # Plugin name
+plugin_dir = None                       # Plugin folder path
+user_folder_dir = None                  # User folder path
+logger = None                           # Instance of class GwLogger. Found in "/lib/tools_log.py"
+project_vars = {}                       # Project variables from QgsProject related to Giswater
+project_vars['info_type'] = None        # gwInfoType
+project_vars['add_schema'] = None       # gwAddSchema
+project_vars['main_schema'] = None      # gwMainSchema
+project_vars['project_role'] = None     # gwProjectRole
+project_vars['project_type'] = None     # gwProjectType
+data_epsg = None                        # SRID retrieved from QGIS project layer "v_edit_node"
+project_epsg = None                     # EPSG of QGIS project
+
+# region Dynamic Variables (variables may change value during user's session)
+session_vars = {}
+session_vars['last_error'] = None          # An instance of the last database runtime error
+session_vars['last_error_msg'] = None      # An instance of the last database runtime error message used in threads
+session_vars['threads'] = []               # An instance of the different threads for the execution of the Giswater functionalities (type:list)
+session_vars['dialog_docker'] = None       # An instance of GwDocker from "/core/ui/docker.py" which is used to mount a docker form
+session_vars['info_docker'] = None         # An instance of current status of the info docker form configured by user. Can be True or False
+session_vars['docker_type'] = None         # An instance of current status of the docker form configured by user. Can be configured "qgis_info_docker" and "qgis_form_docker"
+session_vars['current_selections'] = None  # An instance of the current selections docker.
+session_vars['logged_status'] = None       # An instance of connection status. Can be True or False
+session_vars['last_focus'] = None          # An instance of the last focused dialog's tag
+# endregion
+
+# region global user variables (values are initialized on load project without changes during session)
+user_level = {                          # An instance used to know user level and user level configuration
+    'level': None,                      # initial=1, normal=2, expert=3
+    'showquestion': None,               # Used for show help (default config show for level 1 and 2)
+    'showsnapmessage': None,            # Used to indicate to the user that they can snapping
+    'showselectmessage': None,          # Used to indicate to the user that they can select
+    'showadminadvanced': None,          # Manage advanced tab, fields manager tab and sample dev radio button from admin
+}
+date_format = None                      # Display format of the dates allowed in the forms: dd/MM/yyyy or dd-MM-yyyy or yyyy/MM/dd or yyyy-MM-dd
+# endregion
