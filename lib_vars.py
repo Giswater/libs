bb5eb38ffb2b91a6073dab295f9d66511675cb15
Sergi Maspons <smaspons@bgeo.es>
2023-06-16 13:02:24 +0200

    Add header to lib_vars

--- lib_vars.py
+++ lib_vars.py
@@ -1,3 +1,11 @@
+"""
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+"""
+# -*- coding: utf-8 -*-
+
 schema_name = None                      # Schema name retrieved from QGIS project connection with PostgreSql
 qgis_db_credentials = None              # Instance of class QSqlDatabase (QPSQL) used to manage QTableView widgets
 plugin_name = None                      # Plugin name
