28defd02e564597ad689e7ab0310d17b88046725
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2025-04-15 16:45:01 +0200

    fix: manage translations for QToolBox widgets and fix error when widget is QWidget

--- tools_qt.py
+++ tools_qt.py
@@ -1585,7 +1585,7 @@ def _add_translator(locale_path, log_info=False):
 def _translate_form(dialog, context_name, aux_context='ui_message'):
     """ Translate widgets of the form to current language """
 
-    type_widget_list = [QCheckBox, QGroupBox, QLabel, QPushButton, QRadioButton, QLineEdit, QTextEdit, QTabWidget]
+    type_widget_list = [QCheckBox, QGroupBox, QLabel, QPushButton, QRadioButton, QLineEdit, QTextEdit, QTabWidget, QToolBox]
     for widget_type in type_widget_list:
         widget_list = dialog.findChildren(widget_type)
         for widget in widget_list:
@@ -1681,6 +1681,8 @@ def _translate_tooltip(context_name, widget, idx=None, aux_context='ui_message')
         elif widget.toolTip() in ("", None):
             if type(widget) is QGroupBox:
                 widget.setToolTip(widget.title())
+            elif type(widget) is QWidget:
+                 widget.setToolTip("")
             else:
                 widget.setToolTip(widget.text())
 
